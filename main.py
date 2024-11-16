d9729983a06bbe2293be0684dad5f572c8d3e320
tranthanhnhan <cutin17vn@gmail.com>
2024-11-16 13:15:03 +0700

    Done

---
+++ main.py
@@ -0,0 +1,246 @@
+from typing import Union
+from supabase import create_client, Client
+from fastapi import FastAPI,Depends, HTTPException, status
+import os
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+app = FastAPI()
+
+# ----------------- Authenticate JWT user ----------------- #
+
+from passlib.context import CryptContext
+import jwt
+from datetime import datetime, timedelta
+from fastapi import HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Optional
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "your_secret_key"  # Replace with a secure random key
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1420
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    role_id: int
+    age: int
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def get_user_by_username(username: str):
+    response = supabase.from_("users").select("*").eq("username", username).execute()
+    return response.data[0] if response.data else None
+
+def create_user(user: UserCreate, hashed_password: str):
+    try:
+        response = supabase.table("users").insert({
+            "username": user.username,
+            "hashed_password": hashed_password,
+            "role_id": user.role_id,
+            "age": user.age,
+        }).execute()
+        return response.data
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error when create user: {e}")
+
+@app.post("/users/register", response_model=Token)
+async def register_user(user: UserCreate):
+    if len(user.username) < 5 or len(user.username) > 20:
+        raise HTTPException(status_code=400, detail="Username must be between 5 and 20 characters")
+    existing_user = get_user_by_username(user.username)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    if len(user.password) < 8:
+        raise HTTPException(status_code=400, detail="Password must be at least 8 characters")
+    hashed_password = get_password_hash(user.password)
+    create_user(user, hashed_password)
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/users/login", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = get_user_by_username(form_data.username)
+    if not user or not verify_password(form_data.password, user["hashed_password"]):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    access_token = create_access_token(data={"sub": user["username"]})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+def get_user_role(user_id: int):
+    response = supabase.from_("roles").select("name").eq("role_id", user_id).execute()
+    return response.data[0]["name"] if response.data else None
+
+def get_current_user(token: str = Depends(oauth2_scheme)):    
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        user = get_user_by_username(username)
+        if user is None:
+            raise credentials_exception
+        return user
+    except:
+        raise credentials_exception
+
+# ----------------- Authenticate JWT user ----------------- #
+
+# Get all movies 
+@app.get("/movies")
+def get_movies(page: int = 1):
+    response = supabase.table("movies").select("*").range((page-1)*10, (page*10)-1).execute()
+    return response.data
+
+# Get movie detail by movie id
+@app.get("/movies/{movie_id}/detail")
+def get_movie_detail_by_movie_id(movie_id: int):
+    response = supabase.table("movie_details").select("*").eq("movie_detail_id", movie_id).execute()
+    return response.data
+
+# Get all genres by movie id
+@app.get("/movies/{movie_id}/genres")
+def get_all_genres_by_movie_id(movie_id: int):
+    response = supabase.table("movie_has_genred").select(
+        "genres(*)"
+    ).eq("movie_id", movie_id).execute()
+    
+    return response.data
+
+# Get all actors by movie id
+@app.get("/movies/{movie_id}/actors")
+def get_all_actors_by_movie_id(movie_id: int):
+    response = supabase.table("movie_has_actor").select(
+        "character, actors(*)"
+    ).eq("movie_id", movie_id).execute()
+    
+    return response.data
+
+# Get all comments for movie id
+@app.get("movies/{movie_id}/comments")
+def get_all_comments_by_movie_id(movie_id: int):
+    response = supabase.table("comments").select(
+        "users(*), content"
+    ).eq("movie_id", movie_id).execute()
+    
+    return response.data
+
+# Get all actors
+@app.get("/actors")
+def get_all_actors(page: int = 1):
+    response = supabase.table("actors").select("*").range((page-1)*10, (page*10)-1).execute()
+    return response.data
+
+# Get all movies for that actor
+@app.get("/actors/{actor_id}/movies")
+def get_all_movies_by_actor_id(actor_id: int, page: int = 1):
+    response = supabase.table("movie_has_actor").select(
+      "character, movies(*)"  
+    ).eq("actor_id", actor_id).range((page-1)*10, (page*10)-1).execute()
+    
+    return response.data
+
+# Get all genres
+@app.get("/genres")
+def get_all_genres():
+    response = supabase.table("genres").select("*").execute()
+    return response.data
+
+# Get movies by genre
+@app.get("/genres/{genre_id}/movies")
+def get_all_movies_by_genre_id(genre_id: int, page: int = 1):
+    response = supabase.table("movie_has_genred").select(
+        "movies(*)"
+    ).eq("genre_id", genre_id).range((page-1)*10, (page*10)-1).execute()
+    
+    return response.data
+
+# Add to favorite
+@app.post("users/movies/{movie_id}/favorite")
+def add_to_favorite(movie_id: int, current_user: dict = Depends(get_current_user)):
+    response = supabase.table("favourite_list").insert({
+        "user_id": current_user["user_id"],
+        "movie_id": movie_id
+    }).execute()
+    return response.data
+
+# Delete from favorite
+@app.delete("users/movies/{movie_id}/favorite")
+def delete_from_favorite(movie_id: int, current_user: dict = Depends(get_current_user)):
+    response = supabase.table("favourite_list").delete().eq("user_id", current_user["user_id"]).eq("movie_id", movie_id).execute()
+    return response.data
+
+# Get all favorite movies of the user
+@app.get("users/movies/favorite")
+def get_all_favorite_movies(current_user: dict = Depends(get_current_user)):
+    response = supabase.table("favourite_list").select(
+        "movies(*)"
+    ).eq("user_id", current_user["user_id"]).execute()
+    
+    return response.data
+
+# User Comment for movie
+@app.post("users/movies/{movie_id}/comment")
+def user_comment_for_movie(movie_id: int, comment: str, current_user: dict = Depends(get_current_user)):
+    response = supabase.table("comments").insert({
+        "user_id": current_user["user_id"],
+        "movie_id": movie_id,
+        "content": comment
+    }).execute()
+    return response.data
+
+# User Updated their comment for movie
+@app.patch("users/movies/{movie_id}/comment")
+def user_update_comment_for_movie(movie_id: int, comment: str, current_user: dict = Depends(get_current_user)):
+    response = supabase.table("comments").update({
+        "content": comment,
+        'updated_at': datetime.now()
+    }).eq("user_id", current_user["user_id"]).eq("movie_id", movie_id).execute()
+    return response.data
+
+# Update User watch history
+@app.post("users/movies/{movie_id}/history/watch")
+def update_user_watch_history(movie_id: int, current_user: dict = Depends(get_current_user)):
+    response = supabase.table("watch_histories").insert({
+        "user_id": current_user["user_id"],
+        "movie_id": movie_id
+    }).execute()
+    return response.data
+
+# Get all user watch history
+@app.get("users/movies/history/watch")
+def get_user_watch_history(current_user: dict = Depends(get_current_user)):
+    response = supabase.table("watch_histories").select(
+        "movies(*)"
+    ).eq("user_id", current_user["user_id"]).execute()
+    
+    return response.data
---
+++ table_init.sql
@@ -0,0 +1,106 @@
+-- Table: roles
+CREATE TABLE roles (
+    role_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+-- Table: users
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    hashed_password VARCHAR(100) NOT NULL,
+    role_id INT REFERENCES roles(role_id),
+    age INT NOT NULL
+);
+
+-- Table: genres
+CREATE TABLE genres (
+    genre_id bigint PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+-- Table: movie_details
+CREATE TABLE movie_details (
+    movie_detail_id SERIAL PRIMARY KEY,
+    homepage VARCHAR(255),
+    budget BIGINT,
+    tagline VARCHAR(255),
+    revenue BIGINT,
+    runtime INT,
+    spoken_language text[],
+    status VARCHAR(50),
+    production_countries text[],
+    production_companies text[],
+    video_url VARCHAR(255),
+);
+
+
+-- Table: movies
+CREATE TABLE movies (
+    movie_id bigint PRIMARY KEY,
+    adult BOOLEAN,
+    origin_country VARCHAR(100),
+    original_language VARCHAR(10),
+    original_title VARCHAR(255),
+    overview TEXT,
+    popularity NUMERIC,
+    backdrop_path VARCHAR(255),
+    poster_path VARCHAR(255),
+    release_date DATE,
+    title VARCHAR(255) NOT NULL,
+    vote_average NUMERIC,
+    vote_count INT,
+    movie_detail_id INT REFERENCES movie_details(movie_detail_id)
+);
+
+-- Table: actors
+CREATE TABLE actors (
+    actor_id bigint PRIMARY KEY,
+    known_for_department VARCHAR(50),
+    name VARCHAR(255) NOT NULL,
+    original_name VARCHAR(255),
+    popularity NUMERIC,
+    profile_path VARCHAR(255),
+    gender VARCHAR(10)
+);
+
+-- Table: movie_has_actor
+CREATE TABLE movie_has_actor (
+    movie_id INT REFERENCES movies(movie_id) ON DELETE CASCADE,
+    actor_id INT REFERENCES actors(actor_id) ON DELETE CASCADE,
+    character VARCHAR(255),
+    PRIMARY KEY (movie_id, actor_id)
+);
+
+-- Table: movie_has_genred
+CREATE TABLE movie_has_genred (
+    movie_id INT REFERENCES movies(movie_id) ON DELETE CASCADE,
+    genre_id INT REFERENCES genres(genre_id) ON DELETE CASCADE,
+    PRIMARY KEY (movie_id, actor_id)
+);
+
+-- Table: favourite_list
+CREATE TABLE favourite_list (
+    movie_id INT REFERENCES movies(movie_id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (movie_id, user_id)
+);
+
+-- Table: comments
+CREATE TABLE comments (
+    movie_id INT REFERENCES movies(movie_id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (movie_id, user_id)
+);
+
+-- Table: watch_histories
+CREATE TABLE watch_histories (
+    watch_history_id SERIAL PRIMARY KEY,
+    movie_id INT REFERENCES movies(movie_id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
